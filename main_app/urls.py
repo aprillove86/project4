d15055c78a1d19a29090ef0562b4ff4550abe5c0
Chris Lafortune <chris.lafortune@gmail.com>
2022-03-17 14:01:09 -0400

    working stylesheet, auth, memo crud

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.home, name='home' ),
+    path('about/', views.about, name='about'),
+    path('memos/', views.MemoList.as_view(), name='memos_index'),
+    path('memos/<int:pk>/', views.MemoDetail.as_view(), name='memos_detail'),
+    path('memos/create/', views.MemoCreate.as_view(), name='memos_create'),
+    path('memos/<int:pk>/update/', views.MemoUpdate.as_view(), name='memos_update'),
+    path('memos/<int:pk>/delete/', views.MemoDelete.as_view(), name='memos_delete'),
+    path('accounts/signup/', views.signup, name='signup')
 ]
--- project4/settings.py
+++ project4/settings.py
@@ -123,5 +123,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-'''LOGIN_REDIRECT_URL = '/posts/'
-LOGOUT_REDIRECT_URL = '/'''
+LOGIN_REDIRECT_URL = '/memos/'
+LOGOUT_REDIRECT_URL = '/'
