8f6ffbaa3dcc8255f77f4e1b4755c94d813e141c
Chris Lafortune <chris.lafortune@gmail.com>
2022-03-17 19:58:25 -0400

    messed up models

--- main_app/models.py
+++ main_app/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Memo(models.Model):
     title = models.CharField(max_length=100)
+    date_added = models.DateField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
--- main_app/urls.py
+++ main_app/urls.py
@@ -4,10 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home' ),
     path('about/', views.about, name='about'),
-    path('memos/', views.MemoList.as_view(), name='memos_index'),
-    path('memos/<int:pk>/', views.MemoDetail.as_view(), name='memos_detail'),
-    path('memos/create/', views.MemoCreate.as_view(), name='memos_create'),
-    path('memos/<int:pk>/update/', views.MemoUpdate.as_view(), name='memos_update'),
-    path('memos/<int:pk>/delete/', views.MemoDelete.as_view(), name='memos_delete'),
-    path('accounts/signup/', views.signup, name='signup')
+    
+   
 ]
