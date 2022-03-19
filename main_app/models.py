5cbb2ea1c167cf0bfc1858a0053e250e77099852
Chris Lafortune <chris.lafortune@gmail.com>
2022-03-19 12:11:19 -0400

    wip: FV extracts text between dates

--- main_app/models.py
+++ main_app/models.py
@@ -4,14 +4,17 @@ from django.contrib.auth.models import User
 
 class Memo(models.Model):
     title = models.CharField(max_length=100)
-    date_added = models.DateField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
 
     def __str__(self):
         return f' This memo is about {self.title}'
     
     def get_absolute_url(self):
         return reverse('memos_detail', kwargs={'pk': self.id})
+
+    class Meta:
+        ordering = ('-date', 'title')
 
 
 # Create your models here.
